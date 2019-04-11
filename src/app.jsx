21051cbadc0131e476f388ae6d2b8c25d1c377a1
djoker <djoker@djokers-MacBook-Pro.local>
2019-04-11 23:03:20 +0530

    Initial Commit

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (<div>
+      <h2>Welcome to React!</h2>
+      <h3> created by anoop</h3>
+    </div>);
+  }
+}
